456ce435a95ab95684f171fd96c7ae62543d37cc
Josue <josue_2707@hotmail.com>
2023-03-21 19:43:34 -0500

    Proyecto CRUD Redux

---
+++ src/components/TaskForm.jsx
@@ -0,0 +1,61 @@
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { addTask, editTask } from '../features/tasks/taskSlice';
+import { v4 as uuid } from 'uuid';
+import { useNavigate, useParams } from 'react-router-dom';
+
+export const TaskForm = () => {
+
+  const [task, setTask] = useState({
+    title: '',
+    description: ''
+  })
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const params = useParams();
+
+  const tasks = useSelector((state) => state.tasks)
+
+  const handleChange = (event) => {
+    setTask({
+      ...task,
+      [event.target.name]: event.target.value
+    })
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+
+    if(params.id) {
+      dispatch(editTask(task))
+    } else {
+      dispatch(addTask({
+        ...task,
+        id: uuid(),
+        completed: false
+      }))
+    }
+    
+    navigate('/')
+  }
+
+  useEffect(() => {  
+    if(params.id) {
+      setTask(tasks.find((task) => task.id == params.id))
+    }
+  }, [])
+  
+
+  return (
+    <form onSubmit={handleSubmit} className='bg-zinc-800 max-w-sm p-4 mb-2'>
+      <label htmlFor="title" className='block text-xs font-bold mb-2'>Task:</label>
+      <input type="text" name='title' placeholder='Title' onChange={handleChange} value={task.title} className='w-full p-2 rounded-md bg-zinc-600 mb-2' />
+
+      <label htmlFor="description" className='block text-xs font-bold mb-2'>Description:</label>
+      <textarea className='w-full p-2 rounded-md bg-zinc-600 mb-2' name="description" placeholder='description' onChange={handleChange} value={task.description}></textarea>
+
+      <button className='bg-indigo-600 px-2 py-1' type='submit'>Save</button>
+    </form>
+  )
+}
